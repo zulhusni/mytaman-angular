d02f56948bf71eec8353b2969694a315ec4118df
zulhusni <poknie@gmail.com>
2020-02-19 16:46:58 +0800

    add new folders

---
+++ src/app/visitors/visitors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visitors',
+  templateUrl: './visitors.component.html',
+  styleUrls: ['./visitors.component.css']
+})
+export class VisitorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
